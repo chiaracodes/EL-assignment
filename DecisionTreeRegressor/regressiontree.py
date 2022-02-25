b36e58b9f9aecedc1b0738baf799c797e05e844a
Chiara Palma <chiara.palma@student-cs.fr>
2022-02-25 15:55:57 +0100

    Create regressiontree.py

---
+++ DecisionTreeRegressor/regressiontree.py
@@ -0,0 +1,32 @@
+import pandas as pd 
+import numpy as np 
+
+class TreeNode():
+    def __init__(self) -> None:
+        pass
+    
+    @staticmethod
+    def mse(y_true, y_hat) -> float:
+        pass
+
+    def best_split(self) -> tuple:
+        pass
+    
+    def random_split(self) -> tuple:
+        pass
+
+    def grow_tree(self) -> None:
+        pass
+    
+    def info(self) -> None:
+        '''
+        Print info about the tree
+        '''
+        pass
+
+    def print(self) -> None:
+        '''
+        Print the tree in a nice way
+        '''
+    
+
